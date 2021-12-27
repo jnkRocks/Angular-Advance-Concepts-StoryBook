890628415e92143c7303579c68838ff4072b423e
Jatin Kompelli <JnkRocks@outlook.com>
2021-12-27 22:35:05 +0530

    Added few more classes

--- src/app/demo/components/injection-token/injection-token.component.ts
+++ src/app/demo/components/injection-token/injection-token.component.ts
@@ -11,5 +11,18 @@ export class InjectionTokenComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  info = `Injection token class are used to create unique tokens and are used to inject objects , string etc`;
+   
 
+  js = `
+    export const API_URL= new InjectionToken<string>(''); 
+    providers: [ 
+      { provide: API_URL, useValue: 'http://SomeEndPoint.com/api' }
+    ]
+
+    // To inject it in any component use below code using @inject
+        
+    constructor(@Inject(API_URL) private apiURL: string) { 
+    }
+  `;
 }
